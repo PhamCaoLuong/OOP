6b7ce3ef324c0989b916d6e8b4b3552dfbe84188
PhamCaoLuong <phamcaoluong007@gmail.com>
2017-11-05 09:11:42 +0700

    Update_4
    add Controller Class

---
+++ JumperJumper/JumperJumper/Controller/KeyboardController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+namespace JumperJumper
+{
+    class KeyboardController : IController
+    {
+        private KeyboardState keyboardState;
+        Teno teno;
+        ICommands currentCommand;
+        Dictionary<Keys, ICommands> commandLibrabry;
+
+        public  KeyboardController(Teno teno)
+        {
+            this.teno = teno;
+            commandLibrabry = new Dictionary<Keys, ICommands>();
+            commandLibrabry.Add(Keys.W, currentCommand = new UpCommand(teno));
+            commandLibrabry.Add(Keys.LeftShift, currentCommand = new RunCommand(teno));
+            commandLibrabry.Add(Keys.A, currentCommand = new LeftCommand(teno));
+            commandLibrabry.Add(Keys.D, currentCommand = new RightCommand(teno));
+            commandLibrabry.Add(Keys.S, currentCommand = new DownCommand(teno));
+            commandLibrabry.Add(Keys.Escape, currentCommand = new QuitCommand());
+            commandLibrabry.Add(Keys.Enter, currentCommand = new PauseCommand());
+        }
+
+        public void Update()
+        {
+            currentCommand = new NullCommand();
+            keyboardState = Keyboard.GetState();
+            foreach (Keys key in commandLibrabry)
+            {
+                currentCommand = commandLibrabry[key];
+                currentCommand.Execute();
+            }
+            teno.Idle();
+        }
+    }
+}
